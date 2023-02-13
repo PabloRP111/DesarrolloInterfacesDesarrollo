46c105695691a19b97ff19ae72a68a52929dec96
PabloRP <asraum111@gmail.com>
2023-02-13 14:26:41 +0100

    Funciones Independientes Implementadas

--- EditorImagenes/MainWindow.xaml.cs
+++ EditorImagenes/MainWindow.xaml.cs
@@ -24,6 +24,8 @@ namespace EditorImagenes
 
     public partial class MainWindow : Window
     {
+        private double _scale=1.0;
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             System.Windows.Controls.Button clickedButton = (System.Windows.Controls.Button)sender;
@@ -53,8 +55,19 @@ namespace EditorImagenes
                     string pdfPath = Path.Combine(Application.StartupPath, "GuiaUsuario.pdf");
                     Process.Start(pdfPath);
                     break;
-
-
+                case "bSalir":
+                    this.Close();
+                    break;
+                case "bZoomMas":
+                    _scale += 0.1;
+                    ScaleTransform scaleTransform = new ScaleTransform(_scale, _scale);
+                    imagen.RenderTransform = scaleTransform;
+                    break;
+                case "bZoomMenos":
+                    _scale -= 0.1;
+                    ScaleTransform scaleTransform2 = new ScaleTransform(_scale, _scale);
+                    imagen.RenderTransform = scaleTransform2;
+                    break;
             }
 
 
