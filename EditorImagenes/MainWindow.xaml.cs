21d9ec76106b925910a4f42051ba4db1646d44a0
Alexgar98 <92014559+Alexgar98@users.noreply.github.com>
2023-02-27 18:04:12 +0100

    Mover la imagen con el ratón
    La imagen ahora se puede mover con el ratón en vez de usando la barra. Falla al combinarlo con los botones de zoom y rotación pero parece que es el mismo problema que ya teníamos

--- EditorImagenes/MainWindow.xaml.cs
+++ EditorImagenes/MainWindow.xaml.cs
@@ -25,6 +25,11 @@ namespace EditorImagenes
     public partial class MainWindow : Window
     {
         private double _scale=1.0;
+        private bool _isMoving;
+        private Point? position;
+        private double deltaX;
+        private double deltaY;
+        private TranslateTransform _currentTT;
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -71,6 +76,40 @@ namespace EditorImagenes
             }
 
 
+        }
+
+        //Si se pulsa el ratón
+        private void MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //Se coge la posición del ratón para mover la imagen con él y se setea "isMoving" a true para que MouseMove haga cosas
+            if (position == null)
+                position = imagen.TransformToAncestor(scroll).Transform(new Point(0, 0));
+            var mousePosition = Mouse.GetPosition(scroll);
+            deltaX = mousePosition.X - position.Value.X;
+            deltaY = mousePosition.Y - position.Value.Y;
+            _isMoving = true;
+        }
+
+        //Si se deja de pulsar el ratón
+        private void MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            //Se guarda la posición y se setea el "isMoving" a false para que no la líe si se mueve el ratón
+            _currentTT = imagen.RenderTransform as TranslateTransform;
+            _isMoving = false;
+        }
+
+        //Al mover el ratón
+        private void MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            //Si "isMoving" está en false no hace nada
+            if (!_isMoving) return;
+            //En caso contrario, cambia la posición de la imagen
+            var mousePoint = Mouse.GetPosition(scroll);
+
+            var offsetX = (_currentTT == null ? position.Value.X : position.Value.X - _currentTT.X) + deltaX - mousePoint.X;
+            var offsetY = (_currentTT == null ? position.Value.Y : position.Value.Y - _currentTT.Y) + deltaY - mousePoint.Y;
+
+            this.imagen.RenderTransform = new TranslateTransform(-offsetX, -offsetY);
         }
 
     }
