bace568262804b414e9f4b8e0d23db0f17878689
Jeropis <jerocaru@gmail.com>
2023-03-08 18:48:44 +0100

    Empezando inserts SQL

--- EditorImagenes/datos_autores.sql
+++ EditorImagenes/datos_autores.sql
@@ -1,5 +1,8 @@
 create table autores (
 id integer primary key auto_increment,
 nombre varchar(30) unique not null,
+apellido varchar(30) not null,
 grupo varchar(40) not null
 );
+
+insert into autores (nombre, apellido, grupo) values('Jero');
